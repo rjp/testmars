02c6f2c230ec43ddb2a8dd001ce3fe7c83dcf975
rjp <zimpenfish@gmail.com>
2020-01-16 06:59:55 +0000

    Creating a world

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    w := NewWorld(5, 3)
+    fmt.Printf("%#v\n", w)
+}
---
+++ world.go
@@ -0,0 +1,28 @@
+package main
+
+const MaxX = 50
+const MaxY = 50
+
+// Define types for `Cell` and `Cells` because not only
+// does it help with type safety, it makes the code easier
+// to read for someone else.
+type Cell struct {
+    Scent bool
+}
+
+type Cells []Cell
+
+type World struct {
+    BoundX, BoundY int
+    // An array of array of cells => a 2D array of cell
+    Grid []Cells
+}
+
+func NewWorld(x, y int) World {
+    temp := World{BoundX: x, BoundY: y}
+    temp.Grid = make([]Cells, MaxY)
+    for i:=0; i<MaxY; i++ {
+        temp.Grid[i] = make(Cells, MaxX)
+    }
+    return temp
+}

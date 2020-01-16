ebb3db5e83a46241687a949de068c0b0db2e172b
rjp <zimpenfish@gmail.com>
2020-01-16 07:29:53 +0000

    Robots can move forwards (albeit with no checking)

--- robot.go
+++ robot.go
@@ -13,11 +13,39 @@ func NewRobot(x int, y int, o string, w World) Robot {
     return temp
 }
 
-func (r *Robot) Forward {
 func (r Robot) Position() (x, y int) {
     return r.posX, r.posY
 }
+
+// Translate a given orientation into specific axis changes.
+// In a more complicated scenario, we might model this with
+// a rotational matrix and combine that with a unit matrix to
+// calculate our new position. But this is a simple case and
+// we'll just use a switch for now.
+func (r Robot) TranslateOrientation() (dx, dy int) {
+    var nx, ny int
+
+    switch r.orientation {
+    case "N":
+        nx, ny = 0, 1
+    case "S":
+        nx, ny = 0, -1
+    case "E":
+        nx, ny = 1, 0
+    case "W":
+        nx, ny = -1, 0
+    }
+
+    // We could have used `dx, dy` in the body of this
+    // function instead of `nx, ny` with a bare `return`
+    // but that can be confusing and IMHO explicit is better.
+    return nx, ny
+}
+
 func (r *Robot) Forward() {
+    dx, dy := r.TranslateOrientation()
+    r.posX = r.posX + dx
+    r.posY = r.posY + dy
 }
 
 func (r *Robot) TurnLeft() {
